6a6a3432050753a4a0e02a2f1b75e7eba7196939
Ohuruogu <basil.ohuruogu@thepalladiumgroup.com>
2025-04-22 21:07:53 +0100

    fix:bug

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -4,6 +4,6 @@ export const url =
     : "/api/v1/";
 export const token =
   process.env.NODE_ENV === "development"
-    ? "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzQ1MzUyMTk0fQ.AHO3N4dN4jqvO-ssyQVTO_g1CoaGYb_u78IuLYhVvTsRifudWMalJmGIN8Ka-MtGITCw6gkL8uJDeBqFdqlEYg"
+    ? ""
     : new URLSearchParams(window.location.search).get("jwt");
 
