87ef021a7a8bcb5e165d35f93f338dedac1f4698
Emmanuel Nnajiofor <emmanuel.nnajiofor@thepalladiumgroup.com>
2025-05-26 07:30:05 +0100

    handover commit

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -4,7 +4,7 @@ export const url =
     : "/api/v1/";
 export const token =
   process.env.NODE_ENV === "development"
-    ? "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzQ4MDIwNDYxfQ.kHvcRxLNKC95VVKH2whD9f4fztEhLuROtTRWKtxsrGkvGKXm2FxcPtw6C6TiR9hUwaZWGIf_8zLK768X0ADIiQ"
+    ? "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzQ4MjExOTgxfQ.ceiMAuJi8Xp8hbQ4zhPn7DLmI4QLL4LWzHqLmD2u-wKweapSpuvxkAbWYPMTLuPE8NVyD79043qXEITuHs8LTg"
     : new URLSearchParams(window.location.search).get("jwt");
 
 export const wsUrl = process.env.NODE_ENV === "development"
