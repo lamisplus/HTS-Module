d6d0ff02e606c35f447b2f78035a80ad49ac83bf
taiwo.gboyegun <taiwo.gboyegun@thepalladiumgroup.com>
2025-06-03 03:50:40 +0100

    setup: module setup done

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -1,9 +1,9 @@
 export const url =
-  process.env.NODE_ENV === "development"
-    ? "http://localhost:8789/api/v1/"
-    : "/api/v1/";
+    process.env.NODE_ENV === "development"
+        ? "http://localhost:8789/api/v1/"
+        : "/api/v1/";
 export const token =
-  process.env.NODE_ENV === "development"
-    ? ""
-    : new URLSearchParams(window.location.search).get("jwt");
+    process.env.NODE_ENV === "development"
+        ? "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzQ4OTQzNDkwfQ.8Gw1gNpp4pEsZcnNBVpr3C_TTmJDGtDG1sXEo3L9zYtZ3lYM3_OLFdttLdXbCJj46gT5ksv8Z5OAb8orM2H3Fw"
+        : new URLSearchParams(window.location.search).get("jwt");
 
