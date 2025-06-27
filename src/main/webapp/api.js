d1dff8d578cc10eb5a1ed9cc265c63ee6b2083b9
Taiwo Gboyegun <dtoms624@gmail.com>
2025-06-27 21:03:41 +0100

    Revert PNS required field not to eb be able to save form for offer pns and acce[ted pns no

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -4,6 +4,8 @@ export const url =
         : "/api/v1/";
 export const token =
     process.env.NODE_ENV === "development"
-        ? "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzQ5NjUzNjY0fQ.gF2BdtdE5bkIEeXhlKeBRZ7vWTCCLj2-5gqYqtLXRsdEq7cBzSYlsW0QKQaEyf6sRd-GxmsUBekvz6kziQxLRQ"
+        ? "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzUxMDgyNDE4fQ.JX6yzqJRw8dpFQFunH7UEFpf9L-wOf4eTFao5etWaO41-7IM--uN1q6tNebYAPMa2j7SV1ewySBXVxMlUuZAhQ"
         : new URLSearchParams(window.location.search).get("jwt");
 
+
+        
