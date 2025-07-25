cddd5860fc11d69b6a4324821ba237456b584237
Basil-DataFI <basil.ohuruogu@thepalladiumgroup.com>
2025-07-25 12:51:04 +0100

    JIRA-0000:[Fix] Fixed modal to skip family index form after opt-out from Recency Form

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -4,6 +4,6 @@ export const url =
         : "/api/v1/";
 export const token =
   process.env.NODE_ENV === "development"
-    ? "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzUzMzE5NjQ3fQ.OPGbmIrXWAq0iYQmAipdQr4b7dLqe77EY_A3CuRJoAys6IUZCvau1QC76fgPyIzq1jbXMcILOJUECYraySQZIw"
+    ? ""
     : new URLSearchParams(window.location.search).get("jwt");
 
