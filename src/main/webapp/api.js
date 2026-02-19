9d56fc3888bc6bf4600bd276815cc3db7b15c046
Emmanuel Chinonye Nnajiofor <emmanuel.nnajiofor@thepalladiumgroup.com>
2026-02-19 15:30:20 +0100

    minor changes

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -1,13 +1,13 @@
 export const url =
   process.env.NODE_ENV === "development"
-    ? "http://localhost:8789/api/v1/"
+    ? "http://localhost:8383/api/v1/"
     : "/api/v1/";
 export const token =
   process.env.NODE_ENV === "development"
-    ? "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluLFJERSIsIm5hbWUiOiJHdWVzdCBHdWVzdCIsImV4cCI6MTc2OTM3MzE4Mn0.GqIyI7yriwlqeMq_huWMvlK8dcSdGPllARMx8EkOOEnVnATODQXfsJ0OHT8sh74eMLkNLpPY3QFtiMrKYWqx1Q"
+    ? "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJyZGUtdXNlciIsImF1dGgiOiJTdXBlciBBZG1pbixVc2VyLFJERSIsIm5hbWUiOiJyZGUtdXNlciBsYXN0bmFtZSIsImV4cCI6MTc3MTUxNzI4MX0.VcsayysFYWjPAHqx0NqIE6p-LxxFbicOFvrmXqtSS1-rQ7TDufGQM1JzjThHuoNgtKiDK2D0rnem-Z4mlbFHgQ"
     : new URLSearchParams(window.location.search).get("jwt");
 
 export const wsUrl =
   process.env.NODE_ENV === "development"
-    ? "http://localhost:8789/websocket"
+    ? "http://localhost:8383/websocket"
     : "/websocket";
