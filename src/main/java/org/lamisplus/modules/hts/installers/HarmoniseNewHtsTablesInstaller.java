e406e2efe2a09602b24f3bd9441c44f7d7b1e55e
Emmanuel Chinonye Nnajiofor <emmanuel.nnajiofor@thepalladiumgroup.com>
2026-07-19 19:20:36 +0100

    adjusted changeset to the proper version reslving a bug

--- src/main/java/org/lamisplus/modules/hts/installers/HarmoniseNewHtsTablesInstaller.java
+++ src/main/java/org/lamisplus/modules/hts/installers/HarmoniseNewHtsTablesInstaller.java
@@ -4,9 +4,9 @@ import com.foreach.across.core.installers.AcrossLiquibaseInstaller;
 import org.springframework.core.annotation.Order;
 
 @Order(23)
-@Installer(name = "harmonize-new-hts-tables-new",
+@Installer(name = "harmonize-new-hts-tables-new-bug-resolved",
         description = "Resolves divergent HTS tables",
-        version = 17)
+        version = 18)
 public class HarmoniseNewHtsTablesInstaller extends AcrossLiquibaseInstaller {
     public HarmoniseNewHtsTablesInstaller() {
         super("classpath:installers/hts/harmonise_new_hts_tables.xml");
