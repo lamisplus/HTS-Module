73e291474355798b2e12c8a61f1c1d75e58652e5
Emmanuel Chinonye Nnajiofor <emmanuel.nnajiofor@thepalladiumgroup.com>
2026-07-23 08:18:44 +0100

    finally slved the issue

--- src/main/webapp/api.js
+++ src/main/webapp/api.js
@@ -4,7 +4,7 @@ export const url =
     : "/api/v1/";
 export const token =
   process.env.NODE_ENV === "development"
-    ? "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJyZGUtdXNlciIsImF1dGgiOiJTdXBlciBBZG1pbixVc2VyLFJERSIsImV4cCI6MTc4NDUxMzM4MywibmFtZSI6InJkZS11c2VyIGxhc3RuYW1lIn0.mLGWEKGTmAVuxuWCw9dzx4ajFZbzLhwRSsG7Br7QDQ5HgJmVzzIDWtt6odLRHz_rgXrowEPZMO4EbqZA3U1f0Q"
+    ? "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJyZGUtdXNlciIsImF1dGgiOiJTdXBlciBBZG1pbixVc2VyLFJERSIsImV4cCI6MTc4NDgxOTgwMywibmFtZSI6InJkZS11c2VyIGxhc3RuYW1lIn0.EQvz3OVEzcWLyjtgpYWca5no5qSP7amxZZLxC_0lAzWjTswwsgF5x_yZ8KbNM-3dsAm8oRyhxzp5t05LDCRVVg"
     : new URLSearchParams(window.location.search).get("jwt");
 
 export const wsUrl =
